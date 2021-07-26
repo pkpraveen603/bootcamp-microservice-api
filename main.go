9ae6bb50a1bda13acec8ad4995bc8f03c5806424
pkpraveen603 <pkpraveen603@gmail.com>
2021-07-26 23:42:29 +0530

    Added initial code
    added all initial code, pending logics and updates

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
-
-func main(){
-
+import (
+	"fmt"
+	"github.com/bootcamp-microservice-api/Config"
+	"github.com/bootcamp-microservice-api/Models"
+	"github.com/bootcamp-microservice-api/Routes"
+	"github.com/jinzhu/gorm"
+)
+var err error
+func main() {
+	Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
+	if err != nil {
+		fmt.Println("Status:", err)
+	}
+	defer Config.DB.Close()
+	Config.DB.AutoMigrate(&Models.User{}, &Models.Product{}, &Models.Order{})
+	r := Routes.SetupRouter()
+	//running
+	r.Run()
 }
